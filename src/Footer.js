1e8caeb5b464cc70e5643600219cc49b55898ea3
Sofiyan-Benz <sofiyan_b.s@protonmail.com>
2021-10-29 11:53:13 +0100

    fix bug footer page

--- src/Footer.js
+++ src/Footer.js
@@ -6,42 +6,59 @@ import Facebook from "./images/footer/facebook.png";
 import Linkedin from "./images/footer/linkedin.png";
 
 export default function Footer() {
+    const styleSpan = {
+        lineHeight: "24px",
+        fontSize: "16px",
+        fontStyle: "normal",
+        letterSpacing: "0.2px",
+        fontWeight: 500,
+    };
     return (
-        <div className="bg-black text-white mt-24 pb-28 md:flex justify-around  ">
-            <div className="md:max-w-513 md:flex justify-around space-x-20 ">
+        <div className="bg-black text-white mt-24 pb-28 font-Graphik md:space-x-20 sm:space-y-20  md:flex justify-around sm:pl-28">
+            <div className="md:max-w-513 md:flex md:space-x-20 ">
                 <div className="flex flex-col space-y-7">
                     <h5 className="font-medium text-xl">Fingertipe</h5>
-                    <span>Home</span>
-                    <span>Examples</span>
-                    <span>Pricing</span>
-                    <span>Updates</span>
+                    <span style={styleSpan}>Home</span>
+                    <span style={styleSpan}>Examples</span>
+                    <span style={styleSpan}>Pricing</span>
+                    <span style={styleSpan}> Updates</span>
                 </div>
+
                 <div className="flex flex-col space-y-7">
                     <h5 className="font-medium text-xl">Resources</h5>
-                    <span>Home</span>
-                    <span>Examples</span>
-                    <span>Pricing</span>
-                    <span>Updates</span>
+                    <span style={styleSpan}>Home</span>
+                    <span style={styleSpan}>Examples</span>
+                    <span style={styleSpan}>Pricing</span>
+                    <span style={styleSpan}>Updates</span>
                 </div>
+
                 <div className="flex flex-col space-y-7">
                     <h5 className="font-medium text-xl">About</h5>
-                    <span>Home</span>
-                    <span>Examples</span>
-                    <span>Pricing</span>
-                    <span>Updates</span>
+                    <span style={styleSpan}>Home</span>
+                    <span style={styleSpan}>Examples</span>
+                    <span style={styleSpan}>Pricing</span>
+                    <span style={styleSpan}>Updates</span>
                 </div>
             </div>
 
-            <div className="flex flex-col md:max-w-319">
-                <div className="flex items-center">
-                    <img src={LocationMap} alt="Location Map" />
-                    <span>7480 Mockingbird Hill undefined </span>
+            <div className="flex flex-col md:max-w-319 space-x-2.5 space-y-5">
+                <div className="flex items-center space-x-2">
+                    <img src={LocationMap} alt="Location Map" className="" />
+                    <span style={styleSpan}>
+                        7480 Mockingbird Hill undefined{" "}
+                    </span>
                 </div>
-                <div className="flex items-center ">
-                    <img src={NumberPhone} alt="Number Phone " />
-                    <span>(239) 555-0108 </span>
+
+                <div className="flex items-center space-x-2.5 ">
+                    <img
+                        src={NumberPhone}
+                        alt="Number Phone "
+                        className="w-7"
+                    />
+                    <span style={styleSpan}>(239) 555-0108 </span>
                 </div>
-                <div className="flex items-center">
+
+                <div className="flex items-center space-x-7">
                     <img src={Twitter} alt="icon Twitter" />
                     <img src={Facebook} alt="icon Facebook" />
                     <img src={Linkedin} alt="icon Linkedin" />
