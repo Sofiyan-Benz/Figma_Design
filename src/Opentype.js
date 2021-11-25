1db58a96953b9a33819b5691030f96a47785fc40
Sofiyan-Benz <sofiyan_b.s@protonmail.com>
2021-11-25 01:17:01 +0000

    add font, fix Opentype

--- src/Opentype.js
+++ src/Opentype.js
@@ -7,30 +7,35 @@ export default function Opentype() {
         fontSize: "52px",
         fontStyle: "normal",
         letterSpacing: "0.2px",
-        marginBottom: "10px",
+        marginBottom: "37px",
+        width: "479px",
     };
     return (
-        <div className="bg-black w-full">
-            <div className="bg-secondary  md:rounded-large sm:rounded-none md:flex justify-between md:mr-44 md:ml-44 sm:max-w-549 sm:flex-col sm:flex items-center">
-                <div className="md:max-w-544 sm:max-w-407 sm:flex col sm:flex items-center md:flex flex-col md:flex justify-center ">
+        <div className="bg-black w-full flex justify-center">
+            <div
+                style={{ width: "1219px" }}
+                className="bg-secondary lg:rounded-large sm:rounded-none lg:flex justify-between  "
+            >
+                <div className="lg:pl-24 lg:pr-12 m-auto ">
                     <h3
                         style={StyleTitle}
-                        className="sm:max-w-336 sm:text-center md:max-w-479 md:ml-20 text-white font-normal font-Graphik sm:mt-24  "
+                        className="sm:text-center  text-white font-normal font-Graphik sm:mt-24"
                     >
                         OpenType features and Variable fonts
                     </h3>
                     <button
                         style={{ lineHeight: "25px", fontSize: "17px" }}
-                        className="bg-primary shadow pt-3.5 pb-3.5 pr-16 pl-16 rounded-sm text-white  font-medium  sm:mb-24"
+                        className="bg-primary shadow pt-3.5 pb-3.5 pr-16 pl-16 rounded-sm text-white  font-medium  sm:mb-24 "
                     >
                         Try For Free
                     </button>
                 </div>
-                <div className="">
+
+                <div className=" ">
                     <img
                         src={LineChart}
                         alt=""
-                        className="sm:max-w-541 sm:p-7 md:max-w-585 "
+                        className="sm:p-7  lg:rounded-large "
                     />
                 </div>
             </div>
