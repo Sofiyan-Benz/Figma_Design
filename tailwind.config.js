bdcbbec85925326decc5b5a038dc42aa90b4f8e1
Sofiyan-Benz <sofiyan_b.s@protonmail.com>
2021-12-09 10:43:39 +0000

    purge css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-    purge: [],
+    purge: ["./src/**/*.html", "./src/**/*.js"],
     darkMode: false, // or 'media' or 'class'
     theme: {
         minWidth: { "476": "476px" },
